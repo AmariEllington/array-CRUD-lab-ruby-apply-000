364957688fb971b6c02e58e8d728b52fe8321802
Amari Ellington <amari.l.ellington@gmail.com>
2018-11-25 20:15:43 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array.push ("rat" "rabbit")
-  puts add_element_to_end_of_array
+  add_element_to_end_of_array[4] = "rat"
 
 
 end
