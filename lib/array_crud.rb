27ae7854c85593b03c446880efdcd535e7989a48
Amari Ellington <amari.l.ellington@gmail.com>
2018-12-03 13:29:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,6 +12,10 @@ def add_element_to_end_of_array(array, element)
 
 add_element_to_end_of_array = ["hello"] << "arrays!"
 
+create_an_array << "rat" "rabbit"
+print create_an_array
+
+
 end
 
 def add_element_to_start_of_array(array, element)
@@ -24,9 +28,11 @@ def remove_element_from_end_of_array(array)
 
 remove_element_from_end_of_array = ["hello","arrays!"].pop
 
+
 end
 
 def remove_element_from_start_of_array(array)
+
 
   remove_element_from_end_of_array = ["wow", "arrays!"].shift
 
@@ -38,14 +44,22 @@ def retrieve_element_from_index(array, index_number)
 
 end
 
+def retrieve_element_from_index(array, index_number)
+
+end
+
 def retrieve_first_element_from_array(array)
+
   retrieve_first_element_from_array = ["wow",2,3,]
   retrieve_first_element_from_array[0]
+
 
 end
 
 def retrieve_last_element_from_array(array)
+
   retrieve_last_element_from_array = [1,2,3,"arrays!"]
   retrieve_last_element_from_array[3]
+
 
 end
